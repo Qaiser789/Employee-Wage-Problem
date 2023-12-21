04a16c0f4d6f594e3d09c55c0629e67cda8771c7
Qaiser789 <sutarqaiser@gmail.com>
2023-12-21 12:06:15 +0530

    checking presenting of employees added.

--- src/com/bridgelabz/employeewageprob/EmployeeWage.java
+++ src/com/bridgelabz/employeewageprob/EmployeeWage.java
@@ -1,10 +1,20 @@
 package com.bridgelabz.employeewageprob;
 
+import java.util.Random;
+
+
 public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program.");
+        Random random = new Random();
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program.");
+        // Generate a random number (0 or 1) to represent attendance (0: absent, 1: present)
+        int attendance = random.nextInt(2);
 
-	}
-
+        if (attendance == 1) {
+            System.out.println("Employee is Present.");
+        } else {
+            System.out.println("Employee is Absent.");
+        }
+    }
 }

1526f707eeaccb446eb7d348f04c1f0c71a877c0
Qaiser789 <sutarqaiser@gmail.com>
2023-12-21 12:15:11 +0530

    calulating daily employee wages added.

--- src/com/bridgelabz/employeewageprob/EmployeeWage.java
+++ src/com/bridgelabz/employeewageprob/EmployeeWage.java
@@ -2,17 +2,24 @@ package com.bridgelabz.employeewageprob;
 
 import java.util.Random;
 
-
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program.");
+
         Random random = new Random();
 
-        // Generate a random number (0 or 1) to represent attendance (0: absent, 1: present)
         int attendance = random.nextInt(2);
+
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+
+        int dailyWage = 0;
 
         if (attendance == 1) {
             System.out.println("Employee is Present.");
+
+            dailyWage = wagePerHour * fullDayHour;
+            System.out.println("Daily Employee Wage: " + dailyWage);
         } else {
             System.out.println("Employee is Absent.");
         }

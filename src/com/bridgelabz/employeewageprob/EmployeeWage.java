51dae26011ad52a9104a440984823d34c4aa59eb
Qaiser789 <sutarqaiser@gmail.com>
2023-12-21 12:43:38 +0530

    Total working hours or days reached for month added.

--- src/com/bridgelabz/employeewageprob/EmployeeWage.java
+++ src/com/bridgelabz/employeewageprob/EmployeeWage.java
@@ -12,14 +12,18 @@ public class EmployeeWage {
         int fullDayHour = 8;
         int partTimeHour = 4; // Assuming part-time hours as 4
         int workingDaysPerMonth = 20;
+        int totalWorkingHoursLimit = 100;
 
+        // Initialize variables for monthly employee wage and total working hours
         int monthlyWage = 0;
+        int totalWorkingHours = 0;
+        int currentDay = 0;
 
-        // Loop for each working day in the month
-        for (int day = 1; day <= workingDaysPerMonth; day++) {
+        while (currentDay < workingDaysPerMonth && totalWorkingHours < totalWorkingHoursLimit) {
             // Generate a random number (0, 1, or 2) to represent attendance (0: absent, 1: full-time, 2: part-time)
             int attendance = random.nextInt(3);
 
+            // Variables to store the type of attendance and daily wage
             String attendanceType;
             int dailyWage = 0;
 
@@ -38,13 +42,18 @@ public class EmployeeWage {
             }
 
             // Display the type of attendance and daily wage for the current day
-            System.out.println("Day " + day + ": " + attendanceType + " - Daily Wage: " + dailyWage);
+            System.out.println("Day " + (currentDay + 1) + ": " + attendanceType + " - Daily Wage: " + dailyWage);
 
-            // Accumulate the total monthly wage
+            // Accumulate the total monthly wage and working hours
             monthlyWage += dailyWage;
+            totalWorkingHours += (attendance == 1) ? fullDayHour : (attendance == 2) ? partTimeHour : 0;
+
+            // Move to the next working day
+            currentDay++;
         }
 
-        // Display the monthly employee wage
+        // Display the monthly employee wage and total working hours
         System.out.println("Monthly Employee Wage: " + monthlyWage);
+        System.out.println("Total Working Hours: " + totalWorkingHours);
     }
 }
